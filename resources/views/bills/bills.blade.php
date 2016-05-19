97212fdaff879be5e13cdef6c760a6afb21eefb8
Brandon <schwartzb4@mymacewan.ca>
2016-05-18 18:33:42 -0600

    Added toggle for table column visibility, simplified table creation
    Added toggle for table column visibility, simplified table creation

--- resources/views/bills/bills.blade.php
+++ resources/views/bills/bills.blade.php
@@ -1,16 +1,26 @@
 @extends ('layouts.app')
 
+<?php 
+	$columns = array('Number', 'Date', 'Description', 'Customer', 'Amount', 'Taxes', 'Driver');
+?>
+
 @section ('script')
 
 <script type='text/javascript' src='/DataTables/media/js/jquery.dataTables.min.js'></script>
+<script type='text/javascript' src='/DataTables/extensions/Buttons/js/dataTables.buttons.min.js'></script>
+<script type='text/javascript' src='/DataTables/extensions/Buttons/js/buttons.colVis.js'></script>
 
 <script type='text/javascript'>
 	var table;
 
 	$(document).ready(function(){
-		table = $('#billsTable').DataTable();
-
-	})
+	    table = $('#billsTable').DataTable( {
+	        dom: 'lf<"columnVis"B>rtip',
+	        buttons: [
+	            'colvis'
+	        ]
+	    } );
+	});
 
 </script>
 
@@ -18,14 +28,15 @@
 
 @section ('style')
 
-<link rel='stylesheet' type='text/css' href='/DataTables/media/css/jquery.dataTables.css'>
+<link rel='stylesheet' type='text/css' href='/DataTables/media/css/jquery.dataTables.min.css'>
+<link rel='stylesheet' type='text/css' href='/DataTables/extensions/Buttons/css/buttons.dataTables.min.css'>
 
 @endsection
 
 @section ('navBar')
 
 <table>
-	<button class='navButton btn-primary'>Create New Bill</button>
+	<button class='navButton btn-primary fa'><i class='fa-icon-plus'></i>Create New Bill</button>
 	<button class='navButton btn-primary'>Edit Bill</button>
 	<button class='navButton btn-primary'>Eat IceCream</button>
 </table>
@@ -37,25 +48,17 @@
 <table id='billsTable' class='display'>
 	<thead class="header">
 		<tr>
-			<th>Number</th>
-			<th>Date</th>
-			<th>Description</th>
-			<th>Customer</th>
-			<th>Amount</th>
-			<th>Taxes</th>
-			<th>Driver</th>
+			@foreach($columns as $column)
+				<td>{{$column}}</td>
+			@endforeach
 		</tr>
 	</thead>
 
 	<tbody>
 		<tr>
-			<td>A number goes here</td>
-			<td>A date goes here</td>
-			<td>Description goes here</td>
-			<td>Customer goes here</td>
-			<td>Amount goes here</td>
-			<td>Taxes go here</td>
-			<td>Driver goes here</td>
+			@foreach($columns as $column)
+				<td>{{$column}} goes here </td>
+			@endforeach
 		</tr>
 	</tbody>
 </table>
