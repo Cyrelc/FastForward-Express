d9e864804d1d0b50328052f93bdc0b4cfa2ac2c5
Brandon <schwartzb4@mymacewan.ca>
2017-06-06 14:10:47 -0600

    update fontawesome

--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -17,7 +17,7 @@
         <script src="{{URL::to('/')}}/js/utils.js"></script>
 
         <!-- Fonts -->
-        <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css' rel='stylesheet' type='text/css' />
+        <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet' type='text/css' />
         <link href='https://fonts.googleapis.com/css?family=Lato:100,300,400,700' rel='stylesheet' type='text/css' />
 
         <!-- Styles -->
