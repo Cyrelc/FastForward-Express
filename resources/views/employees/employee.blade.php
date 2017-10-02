71e40cf4083211424b370c9498b7be9b3d718d5a
Brandon <schwartzb4@mymacewan.ca>
2017-10-02 16:36:17 -0600

    increase stability of employee creation view

--- resources/views/employees/employee.blade.php
+++ resources/views/employees/employee.blade.php
@@ -6,28 +6,7 @@
 <script type="text/javascript" src="{{URL::to('/')}}/js/bootstrap-datetimepicker.min.js"></script>
 <script type="text/javascript" src="https://nosir.github.io/cleave.js/dist/cleave.min.js"></script>
 <script type="text/javascript" src="https://nosir.github.io/cleave.js/js/lib.js"></script>
-
-<script type="text/javascript">
-    $(document).ready(function(){
-        dateInput('dob-picker');
-        dateInput('startdate-picker');
-
-		$("#sin").keydown(function(e){numberFilter(e);});
-
-        new Cleave('#sin', {
-            delimiter: ' ',
-            blocks: [3, 3, 3]
-        });
-
-		@if(!empty($errors) && $errors->count() > 0)
-			var active = '{{old('active')}}';
-			if (active == '') {
-				$("#chkActive").attr('checked', false);
-			}
-		@endif
-	});
-
-</script>
+<script type="text/javascript" src="{{URL::to('/')}}/js/employees/employee.js"></script>
 
 @parent
 
@@ -49,52 +28,60 @@
 
 <form onsubmit="" method="POST" action="/employees/store">
 	<input type="hidden" name="_token" value="{{ csrf_token() }}">
+	<input id="is_driver" name="is_driver" value="{{isset($model->driver->driver_id) ? "true" : "false" }}">
+	<input type="hidden" id="is_sales" name="is_sales" value="">
 
 	@if(isset($model) && isset($model->employee) && $model->employee->employee_id > 0)
 		<input type="hidden" name="employee_id" value="{{$model->employee->employee_id}}"/>
 		<input type="hidden" name="user_id" value="{{$model->employee->user_id}}"/>
 	@endif
-
-	<div class="well">
-		<div class="clearfix">
 <!-- errors will be output here -->
-			@if(!empty($errors) && $errors->count() > 0)
-				<div class="row">
-					<div class="col-lg-12">
-						<div class="alert alert-danger">
-							<p>The following errors occurred on submit:</p>
+	@if(!empty($errors) && $errors->count() > 0)
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="alert alert-danger">
+					<p>The following errors occurred on submit:</p>
 
-							<ul>
-								@foreach($errors->all() as $message)
-									<!--Custom Messages-->
-										@if ($message === "The contacts field is required.")
-											<li>At least one emergency contact must be provided.</li>
-										@elseif ($message === "The contact- action field is required.")
-											<li>An error has occurred. Please contact us and provide the following message: <pre>Contact Action not submitted.</pre></li>
-										@else
-											<li>{{ $message }}</li>
-										@endif
-								@endforeach
-							</ul>
+					<ul>
+						@foreach($errors->all() as $message)
+							<!--Custom Messages-->
+								@if ($message === "The contacts field is required.")
+									<li>At least one emergency contact must be provided.</li>
+								@elseif ($message === "The contact- action field is required.")
+									<li>An error has occurred. Please contact us and provide the following message: <pre>Contact Action not submitted.</pre></li>
+								@else
+									<li>{{ $message }}</li>
+								@endif
+						@endforeach
+					</ul>
+				</div>
+			</div>
+		</div>
+	@endif
+	<p id='errors'></p>
+	<div class="col-lg-12">
+		<ul class='nav nav-pills'>
+			<li class="active"><a data-toggle="pill" href="#main"> Main </a></li>
+			<li id="driver_form_button" name="driver_form_button" class="hidden"><a data-toggle="pill" href="#driver"> Driver</a></li>
+			<li id="sales_form_button" class="hidden"><a data-toggle="pill" href="#sales"> Sales </a></li>
+		</ul>
+	</div>
+	<div class="tab-content">
+		<div id="main" class="tab-pane fade in active">
+<!--Contact Info Panel-->
+				<div class="col-lg-12">
+					<div class='panel panel-default'>
+						<div class='panel-heading'>
+							<h3 class='panel-title'>Contact Info</h3>
+						</div>
+						<div class='panel-body'>
+							@include('partials.contact', ['prefix' => 'contact', 'show_address' => true, 'contact' => $model->contact])
 						</div>
 					</div>
 				</div>
-			@endif
-			<p id='errors'></p>
-<!--Contact Info Panel-->
-			<div class="col-lg-12">
-				<div class='panel panel-default'>
-					<div class='panel-heading'>
-						<h3 class='panel-title'>Contact Info</h3>
-					</div>
-					<div class='panel-body'>
-						@include('partials.contact', ['prefix' => 'contact', 'show_address' => true, 'contact' => $model->contact])
-					</div>
-				</div>
-			</div>
 
 <!--Emergency Contacts-->
-			@include('partials.contacts', ['contacts' => $model->emergency_contacts, 'show_address' => true, 'title' => 'Emergency Contacts', 'prefix' => 'sc'])
+				@include('partials.contacts', ['contacts' => $model->emergency_contacts, 'show_address' => true, 'title' => 'Emergency Contacts', 'prefix' => 'sc'])
 
 <!--Additional Info Panel-->
 			<div class="col-lg-12">
@@ -138,6 +125,91 @@
 				</div>
 			</div>
 		</div>
+		<div id='driver' class='tab-pane fade'>
+			<input type="hidden" name="driver_id" value="{{$model->driver->driver_id}}"/>
+
+			<div class="col-lg-12">
+				<div class="panel panel-default">
+					<div class='panel-heading'>
+						<h3 class='panel-title'>Driver Information</h3>
+					</div>
+					<div class='panel-body'>
+<!--Driver's License-->
+<!--DLN-->
+						<div class="col-lg-6 well bottom15">
+							<div class="input-group bottom15">
+								<span class="input-group-addon"><i class="fa fa-id-card-o"></i> Drivers License Number</span>
+								<input type="text" id="dln" name="DLN" class="form-control dln" placeholder="Drivers License Number" value="{{$model->driver->drivers_license_number}}"/>
+							</div>
+<!--License Expiration-->
+							<div class='input-group' id='license-picker'>
+								<span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i>&nbsp&nbspExpiration Date</span>
+								<input type='text' name="license_expiration" class="form-control" placeholder="Drivers License Expiration Date" value="{{date("l, F d Y", $model->driver->license_expiration)}}"/>
+							</div>
+						</div>
+<!--Pickup Commission-->
+						<div class="col-lg-6 bottom15">
+							<div class="input-group bottom15">
+								<input type="number" name="pickup-commission" class="form-control" placeholder="Pickup Commission" value="{{$model->driver->pickup_commission}}"/>
+								<span class="input-group-addon">%</span>
+							</div>
+<!--Delivery Commission-->
+							<div class="input-group">
+								<input type="number" name="delivery-commission" class="form-control" placeholder="Delivery Commission" value="{{$model->driver->delivery_commission}}"/>
+								<span class="input-group-addon">%</span>
+							</div>
+						</div>
+<!--Pager Number-->
+						<div class="col-lg-4 bottom15">
+							@include('partials.phone_number', ['prefix' => 'pager', 'phone' => $model->contact->pager, 'placeholder'=>'Pager', 'isPrimary'=>false])
+						</div>
+					</div>
+				</div>
+				<div class="panel panel-default">
+					<div class='panel-heading'>
+						<h3 class='panel-title'> Vehicle Information </h3>
+					</div>
+					<div class='panel-body'>
+<!--License Plate-->
+						<div class="col-lg-6">
+							<div class="well">
+<!--License Plate-->
+								<div class="input-group bottom15">
+									<span class="input-group-addon"><i class="fa fa-car"></i>&nbsp&nbspLicense Plate</span>
+									<input type="text" id="lp" name="license_plate" class="form-control" placeholder="License Plate" value="{{$model->driver->license_plate_number}}"/>
+								</div>
+
+<!--License Plate Expiration-->
+								<div id="license_plate_expiration" class="bottom15">
+									<div class="input-group" id="lp-picker">
+										<span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i>Expiration Date</span>
+										<input type='text' name="license_plate_expiration" class="form-control" placeholder="License Plate Expiration Date" value="{{date("l, F d Y", $model->driver->license_plate_expiration)}}"/>
+									</div>
+								</div>
+							</div>
+						</div>
+
+<!--Insurance-->
+						<div class="col-lg-6">
+							<div class="well">
+<!--Insurance Number-->
+								<div class="input-group bottom15">
+									<span class="input-group-addon"><i class="fa fa-road"></i>&nbsp&nbspInsurance Number</span>
+									<input type="text" name="insurance" class="form-control" placeholder="Insurance Number" value="{{$model->driver->insurance_number}}"/>
+								</div>
+<!--Insurance Expiration-->
+								<div id="insurance_expiration" class="bottom15">
+									<div class='input-group date' id='insurance-picker'>
+										<span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i>&nbsp&nbspExpiration Date</span>
+										<input type='text' name="insurance_expiration" class="form-control" placeholder="Insurance Expiration Date" value="{{date("l, F d Y", $model->driver->insurance_expiration)}}"/>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 	<div class='text-center'>
 		<button type='submit' class='btn btn-primary'>Submit</button>
@@ -145,6 +217,15 @@
 </form>
 @endsection
 
-@section ('navBar')
-
+@section ('advFilter')
+<div class="well form-group" id="job_types">
+    <h4>Employee Roles</h4>
+    <hr>
+    <div class="checkbox">
+        <label><input type="checkbox" id="is_driver_checkbox" {{$model->driver->driver_id == null ? "" : "checked" }} />Driver</label>
+    </div>
+    <div class="checkbox">
+    	<label><input disabled type="checkbox" id="is_sales_checkbox" />Sales</label>
+    </div>
+</div>
 @endsection
