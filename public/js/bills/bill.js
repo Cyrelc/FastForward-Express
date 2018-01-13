540a9422c01af3098d99d27ce3416885f23639e4
Brandon <schwartzb4@mymacewan.ca>
2018-01-13 10:05:37 -0700

    added missing error response to bill ajax submission

--- public/js/bills/bill.js
+++ public/js/bills/bill.js
@@ -176,6 +176,13 @@ function storeBill(){
 					'onHidden': function(){location.reload()}
 				})
 			}
+		},
+		'error': function(response){
+			var errorText = '';
+			for(var key in response.responseJSON){
+				errorText += response.responseJSON[key][0] + '</br>';
+			}
+			toastr.error(errorText, 'Errors', {'timeOut' : 0, 'extendedTImeout' : 0});
 		}
 	})
 }
