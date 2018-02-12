c4e9530332d83adc9b2ebcce8ef1fcb4f4b66c3f
Brandon <schwartzb4@mymacewan.ca>
2018-02-12 09:37:43 -0700

    add new chargebacks table migration

---
+++ database/migrations/2018_02_12_090013_create_chargebacks_v2.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChargebacksV2 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chargebacks', function (Blueprint $table) {
+            $table->increments('chargeback_id');
+            $table->unsignedInteger('employee_id');
+            $table->unsignedInteger('manifest_id')->nullable();
+            $table->float('amount');
+            $table->string('gl_code');
+            $table->string('name');
+            $table->text('description');
+            $table->boolean('continuous')->default(false);
+            $table->unsignedInteger('count_remaining');
+
+			$table->foreign('manifest_id')->references('manifest_id')->on('manifests');
+			$table->foreign('employee_id')->references('employee_id')->on('employees');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('chargebacks');
+    }
+}
