2c80bf019a018aa97f6364bbef2f1300fe70d806
Brandon <schwartzb4@mymacewan.ca>
2018-02-25 08:54:32 -0700

    Fix bill collector to accurately store driver commission to fit new format. Update welcome page to reflect changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 
+@section('style')
+<link rel='stylesheet' type='text/css' href='/css/welcome/welcome.css' />
+@parent
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row">
@@ -8,7 +13,36 @@
                 <div class="panel-heading">Welcome</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    <table>
+                        <thead>
+                            <tr>
+                                <td>Version</td>
+                                <td>Info</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1.143</td>
+                                <td>
+                                    <ul>
+                                        <li>Fix bill pickup/delivery commission</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>1.142</td>
+                                <td>
+                                    <ul>
+                                        <li>Manifest creation view added</li>
+                                        <li>Manifest preview basic view added</li>
+                                        <li>Added logic for creation of and management of Manifests</li>
+                                        <li>Changed pickup and delivery drivers from optional to mandatory fields on bill object</li>
+                                        <li>Adjusted manifests table to accurately reflect date created</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
