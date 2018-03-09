3a76947325fac50a3d8535ed3f25e4b03dba355b
Brandon <schwartzb4@mymacewan.ca>
2018-03-08 21:19:31 -0700

    upgrade font awesome, add link to manifests table view

--- public/js/manifests/manifests.js
+++ public/js/manifests/manifests.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
         ajax: {url:'/manifests/buildTable', dataSrc:''},
         dom: 'lf<"columnVis"B>rtip',
         buttons: ['colvis'],
-        columnDefs: [{'sWidth':'25px', 'aTargets':[0]}],
+        columnDefs: [{'sWidth':'30px', 'aTargets':[0]}],
         pageLength: 50,
         order: [1, 'desc'],
         createdRow: function (row, data, index) {
-            var editButton = '<a class="fa fa-edit btn btn-default btn-xs" title="View Manifest" href="/manifests/view/' + data.manifest_id + '" /></button>';
-            var deleteButton = '<a class="fa fa-trash btn-danger btn-xs" title="Delete Manifest" onclick="deleteManifest(' + data.manifest_id + '" /></button>';
+            var editButton = '<a class="fa fa-edit btn btn-default btn-xs" title="View Manifest" href="/manifests/view/' + data.manifest_id + '" />';
+            var deleteButton = '<a class="fa fa-trash-alt btn btn-danger btn-xs" title="Delete Manifest" onclick="deleteManifest(' + data.manifest_id + '" />';
             $actiontd = $('td', row).eq(0);
             $actiontd.html('<div class="hover-div" >' + editButton + deleteButton + '</div>');
             $('td', row).eq(4).html('<a href="/employees/edit/' + data.employee_id + '" >' + data.employee_name + '</a>');
