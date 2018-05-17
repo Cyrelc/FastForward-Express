27e58ac3bc6941e43d5ed1516fd7f1b4636a57a1
Brandon <schwartzb4@mymacewan.ca>
2018-05-17 11:16:23 -0600

    cache bust of javascript file for accounts.blade.php

--- resources/views/accounts/accounts.blade.php
+++ resources/views/accounts/accounts.blade.php
@@ -4,7 +4,7 @@
 <script type='text/javascript' src='/DataTables/media/js/jquery.dataTables.min.js'></script>
 <script type='text/javascript' src='/DataTables/extensions/Buttons/js/dataTables.buttons.min.js'></script>
 <script type='text/javascript' src='/DataTables/extensions/Buttons/js/buttons.colVis.js'></script>
-<script type='text/javascript' src='/js/accounts/accounts.js'></script>
+<script type='text/javascript' src='/js/accounts/accounts.js?17-05-2018'></script>
 <script type="text/javascript" src='/js/toastr.min.js'> </script>
 @endsection
 
