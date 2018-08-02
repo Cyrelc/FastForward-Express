11fde7190c7f8ed2a4bd75c16bd0dbc4f6d7fb06
Brandon <schwartzb4@mymacewan.ca>
2018-08-02 12:47:22 -0600

    clean up non-functional commissions view

--- resources/views/accounts/commissions.blade.php
+++ resources/views/accounts/commissions.blade.php
@@ -2,25 +2,9 @@
     <div class="col-lg-12">
         <div class="panel panel-default">
             <div class="panel-heading">
-                <h3 class="panel-title">Basic Info</h3>
+                <h3 class="panel-title">Commissions</h3>
             </div>
             <div class='panel-body'>
-<!-- Commissions -->
-                <div class="col-lg-12 col-nopadding">
-                    @include('partials.commission', [
-                        'commission' => count($model->commissions) > 0 ? $model->commissions[0] : null,
-                        'prefix' => 'commission-1',
-                        'employees' => $model->employees,
-                        'title' => 'Commission 1'
-                    ])
-
-                    @include('partials.commission', [
-                        'commission' => count($model->commissions) > 1 ? $model->commissions[1] : null,
-                        'prefix' => 'commission-2',
-                        'employees' => $model->employees,
-                        'title' => 'Commission 2'
-                    ])
-                </div>
             </div>
         </div>
     </div>
