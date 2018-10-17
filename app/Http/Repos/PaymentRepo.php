be0c0907c50367ffc95cbc4d9e498389dee5d541
Brandon <schwartzb4@mymacewan.ca>
2018-10-17 09:57:20 -0600

    show payment amount to 2 decimal places

--- app/Http/Repos/PaymentRepo.php
+++ app/Http/Repos/PaymentRepo.php
@@ -7,7 +7,14 @@ use Illuminate\Support\Facades\DB;
 class PaymentRepo {
 
     public function listPaymentsByAccount($account_id) {
-        $payments = Payment::where('account_id', $account_id);
+        $payments = Payment::where('account_id', $account_id)
+            ->select('payment_id',
+                    'invoice_id',
+                    'date',
+                    DB::raw('format(amount, 2) as amount'),
+                    'payment_type',
+                    'reference_value',
+                    'comment');
 
         return $payments->get();
     }
