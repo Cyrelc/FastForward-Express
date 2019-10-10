276afd087882065145bda94b545aada6d26c6042
Brandon <schwartzb4@mymacewan.ca>
2019-10-10 09:09:13 -0600

    cleanup of database, alphabetization of bills, updates to match live database structure

--- database/migrations/2016_08_09_003820_create_addresses_table.php
+++ database/migrations/2016_08_09_003820_create_addresses_table.php
@@ -15,15 +15,15 @@ class CreateAddressesTable extends Migration
         Schema::create('addresses', function (Blueprint $table) {
             $table->increments('address_id');
             $table->string('name')->nullable();
-            $table->string('formatted_address');
+            $table->string('formatted');
             $table->float('lat')->nullable();
             $table->float('lng')->nullable();
-            $table->string('street');
-            $table->string('street2');
-            $table->string('city');
-            $table->string('zip_postal');
-            $table->string('state_province');
-            $table->string('country');
+            $table->string('street'); //to be deprecated
+            $table->string('street2'); //to be deprecated
+            $table->string('city'); //to be deprecated
+            $table->string('zip_postal'); //to be deprecated
+            $table->string('state_province'); //to be deprecated
+            $table->string('country'); //to be deprecated
             $table->boolean('is_primary')->default(true);
             $table->unsignedInteger('contact_id')->nullable();
 
---
+++ database/migrations/2019_10_09_202257_create_payment_types_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_types', function (Blueprint $table) {
+            $table->increments('payment_type_id');
+
+            $table->string('name');
+            $table->string('required_field')->nullable()->default(null);
+            $table->unsignedInteger('default_ratesheet_id');
+
+            $table->foreign('default_ratesheet_id')->references('ratesheet_id')->on('ratesheets');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_types');
+    }
+}
