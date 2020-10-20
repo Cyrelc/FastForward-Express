1c60b58948e2611dada889fb3dbb3d8edee00cee
Brandon <schwartzb4@mymacewan.ca>
2020-10-20 14:25:43 -0600

    removing unnecessary views

---
+++ resources/views/guest/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class='container'>
+    <div class='row'>
+        <div class='col-md-8 col-md-offset-2'>
+            <div class='panel panel-default'>
+                <div class='panel-heading'>About the company</div>
+            </div>
+        </div>
+    </row>
+</div>
+@endsection
+
