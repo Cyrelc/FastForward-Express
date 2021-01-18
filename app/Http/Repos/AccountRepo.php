6f764532906f6cf1aa147f633a6c39f0870e1599
Brandon <schwartzb4@mymacewan.ca>
2021-01-18 10:57:00 -0700

    fix for duplicate account entries as primary account user is not currently an accurate state

--- app/Http/Repos/AccountRepo.php
+++ app/Http/Repos/AccountRepo.php
@@ -148,7 +148,8 @@ class AccountRepo {
             ->leftJoin('selections as invoice_intervals', 'invoice_intervals.value', '=', 'accounts.invoice_interval')
             ->leftJoin('account_users', function($join) {
                 $join->on('account_users.account_id', '=', 'accounts.account_id')
-                    ->where('account_users.is_primary', '=', 1);
+                    ->where('account_users.is_primary', '=', 1)
+                    ->limit(1);
             })
             ->leftJoin('contacts', 'account_users.contact_id', '=', 'contacts.contact_id')
             ->leftJoin('phone_numbers', function($join) {
