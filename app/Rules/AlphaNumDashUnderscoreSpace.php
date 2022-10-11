70adae7293e2fe1fe3eddfc1a70931afd501cd61
Brandon <schwartzb4@mymacewan.ca>
2022-10-11 14:43:17 -0600

    escape dash character in regex

--- app/Rules/AlphaNumDashUnderscoreSpace.php
+++ app/Rules/AlphaNumDashUnderscoreSpace.php
@@ -25,7 +25,7 @@ class AlphaNumDashUnderscoreSpace implements Rule
      */
     public function passes($attribute, $value)
     {
-        return preg_match('/^[a-zA-Z0-9\s-_]+$/', $value);
+        return preg_match('/^[a-zA-Z0-9\s\-_]+$/', $value);
     }
 
     /**
