9c16373cb08d91fcda77d5f6fb42e054f3357efa
Brandon <schwartzb4@mymacewan.ca>
2023-04-05 12:44:51 -0600

    fix issue with populating datetime filter fields related to DST

--- resources/assets/js/components/tableFilters/DateBetweenFilter.js
+++ resources/assets/js/components/tableFilters/DateBetweenFilter.js
@@ -1,89 +1,67 @@
-import React, {Component} from 'react'
+import React, {useEffect, useState} from 'react'
 import {Col, InputGroup} from 'react-bootstrap'
 import DatePicker from 'react-datepicker'
+import {DateTime} from 'luxon'
 
-export default class DateFilterBetween extends Component {
-    constructor() {
-        super()
-        this.state = {
-            endDate: null,
-            startDate: null,
-            // filterString: '',
-            dbField: ''
-        }
-        this.handleDateFilterChange = this.handleDateFilterChange.bind(this)
-    }
+const DateBetweenFilter = (props) => {
+    const [endDate, setEndDate] = useState(null)
+    const [startDate, setStartDate] = useState(null)
 
-    componentDidMount() {
+    useEffect(() => {
         var dates = null
-        if(window.location.search.includes('filter[' + this.props.filter.value + ']=')) {
-            //get everything between 'filter[db_field_name]=' and the next filter (if there is one) beginning with &
-            const filterValue = window.location.search.split('[' + this.props.filter.value + ']=')[1].split('&')[0]
+        if(window.location.search.includes(`filter[${props.filter.value}]=`)) {
+            const filterValue = window.location.search.split(`[${props.filter.value}]=`)[1].split('&')[0]
             dates = filterValue.split(',')
         }
         if(dates) {
-            const timezoneOffset = new Date().getTimezoneOffset();
-            const startDate = new Date(dates[0]).addMinutes(timezoneOffset)
-            const endDate = new Date(dates[1]).addMinutes(timezoneOffset)
-            this.setState({
-                dbField: this.props.filter.value,
-                endDate: dates[1] ? endDate : null,
-                startDate: dates[0] ? startDate : null
-            }, () => this.handleDateFilterChange())
+            if(dates[0])
+                setStartDate(DateTime.fromFormat(dates[0], 'yyyy-mm-dd').toJSDate())
+            if(dates[1])
+                setEndDate(DateTime.fromFormat(dates[1], 'yyyy-mm-dd').toJSDate())
         }
-        else
-            this.setState({
-                dbField: this.props.filter.value,
-            }, () => this.handleDateFilterChange())
-    }
+    }, [])
 
-    handleDateFilterChange(date = null, type = null) {
-        const startDate = type === 'startDate' ? date : this.state.startDate
-        const endDate = type === 'endDate' ? date : this.state.endDate
-        const formattedStartDate = startDate === null ? '' : new Date(startDate).toISOString().split('T')[0]
-        const formattedEndDate = endDate === null ? '' : new Date(endDate).toISOString().split('T')[0]
-        this.setState({startDate: startDate, endDate: endDate})
-        var filterQueryString = null
-        if(formattedStartDate || formattedEndDate)
-            filterQueryString = 'filter[' + this.state.dbField + ']=' + formattedStartDate + (endDate ? ',' + formattedEndDate : '')
+    useEffect(() => {
+        const formattedStartDate = startDate ? DateTime.fromJSDate(startDate).toFormat('yyyy-mm-dd') : ''
+        const formattedEndDate = endDate ? DateTime.fromJSDate(endDate).toFormat('yyyy-mm-dd') : ''
+        const filterQueryString = `filter[${props.filter.value}]=${formattedStartDate}${endDate ? ',' + formattedEndDate : ''}`
 
-        this.props.handleFilterQueryStringChange({target: {name: this.props.filter.value, type: 'string', value: filterQueryString}})
-    }
+        props.handleFilterQueryStringChange({target: {name: props.filter.value, type: 'string', value: filterQueryString}})
+    }, [startDate, endDate])
 
-    render() {
-        return(
-            <Col md={6}>
-                <InputGroup>
-                    <InputGroup.Text>{this.props.filter.name} Between: </InputGroup.Text>
-                    <DatePicker
-                        wrapperClassName='form-control'
-                        className='form-control'
-                        dateFormat='MMMM d, yyyy'
-                        endDate={this.state.endDate}
-                        isClearable
-                        placeholderText='After'
-                        selectsStart
-                        selected={this.state.startDate}
-                        onChange={date => this.handleDateFilterChange(date, 'startDate')}
-                        wrapperClassName='form-control'
-                    />
-                    <InputGroup.Text>And</InputGroup.Text>
-                    <DatePicker
-                        wrapperClassName='form-control'
-                        className='form-control'
-                        dateFormat='MMMM d, yyyy'
-                        isClearable
-                        placeholderText='Before'
-                        minDate={this.state.startDate}
-                        selected={this.state.endDate}
-                        selectsEnd
-                        startDate={this.state.startDate}
-                        onChange={date => this.handleDateFilterChange(date, 'endDate')}
-                        wrapperClassName='form-control'
-                    />
-                </InputGroup>
-            </Col>
-        )
-    }
+    return(
+        <Col md={6}>
+            <InputGroup>
+                <InputGroup.Text>{props.filter.name} Between: </InputGroup.Text>
+                <DatePicker
+                    wrapperClassName='form-control'
+                    className='form-control'
+                    dateFormat='MMMM d, yyyy'
+                    endDate={endDate}
+                    isClearable
+                    placeholderText='After'
+                    selectsStart
+                    selected={startDate}
+                    onChange={setStartDate}
+                    wrapperClassName='form-control'
+                />
+                <InputGroup.Text>And</InputGroup.Text>
+                <DatePicker
+                    wrapperClassName='form-control'
+                    className='form-control'
+                    dateFormat='MMMM d, yyyy'
+                    isClearable
+                    placeholderText='Before'
+                    minDate={startDate}
+                    selected={endDate}
+                    selectsEnd
+                    startDate={startDate}
+                    onChange={setEndDate}
+                    wrapperClassName='form-control'
+                />
+            </InputGroup>
+        </Col>
+    )
 }
 
+export default DateBetweenFilter;
