c38014a604fbee748a3f03cd7a489bb10ffe6b85
Brandon <schwartzb4@mymacewan.ca>
2023-07-06 11:55:47 -0600

    only prompt to change line item driver if line item has been assigned already

--- resources/assets/js/components/bills/Bill.js
+++ resources/assets/js/components/bills/Bill.js
@@ -324,28 +324,17 @@ const Bill = (props) => {
         if(!pickupDriver || !deliveryDriver)
             return
 
-        let requiresConfirmation = false
-
         const mismatchedCharges = charges.filter(charge =>
             charge.lineItems.some(lineItem => {
-                if(lineItem.delivery_manifest_id == null && lineItem.delivery_driver_id != deliveryDriver.employee_id) {
-                    if(lineItem.delivery_driver_id != null)
-                        requiresConfirmation = true
+                if(lineItem.delivery_manifest_id == null && lineItem.delivery_driver_id && lineItem.delivery_driver_id != deliveryDriver.employee_id)
                     return true
-                }
-                if(lineItem.pickup_manifest_id == null && lineItem.pickup_driver_id != pickupDriver.employee_id) {
-                    if(lineItem.pickup_driver_id != null)
-                        requiresConfirmation = true
+                if(lineItem.pickup_manifest_id == null && lineItem.pickup_driver_id && lineItem.pickup_driver_id != pickupDriver.employee_id)
                     return true
-                }
                 return false
             }
         ))
 
-        if(mismatchedCharges.length) {
-            if(requiresConfirmation && !confirm(unmanifestedDriverMismatchMessage))
-                return
-
+        if(mismatchedCharges.length && confirm(unmanifestedDriverMismatchMessage)) {
             charges.forEach((charge, index) => {
                 const updatedLineItems = charge.lineItems.map(lineItem => {
                     return {
