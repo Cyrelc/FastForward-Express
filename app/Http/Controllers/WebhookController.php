a174df79f4df0e5680904d97d8392a0b559f425b
Brandon <schwartzb4@mymacewan.ca>
2023-09-06 14:49:47 -0600

    remove activity log lines

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -10,8 +10,6 @@ use App\Http\Repos;
 
 class WebhookController extends Controller {
     public function HandlePaymentIntentUpdate(Request $req) {
-        // \Stripe\Stripe::setApiKey(env('MIX_STRIPE_KEY'));
-
         try {
             $event = \Stripe\Webhook::constructEvent($req->getContent(), $req->header('Stripe-Signature'), env('STRIPE_WEBHOOK_SECRET'));
         } catch (\Exception $e) {
@@ -20,13 +18,10 @@ class WebhookController extends Controller {
 
         $invoiceRepo = new Repos\InvoiceRepo();
         $paymentRepo = new Repos\PaymentRepo();
-        activity('stripe_webhook')->log('event type: ', gettype($event));
 
         $paymentIntent = $event->data->object;
         switch($event->type) {
             case 'payment_intent.succeeded':
-                activity('stripe_webhook')->log('payment_intent = ' . $paymentIntent);
-                activity('stripe_webhook')->log('payment_intent_id = ' . $paymentIntent->id);
                 $payment = $paymentRepo->GetPaymentByPaymentIntentId($paymentIntent->id);
                 if(!$payment || $payment->account_id)
                     break;
