b68e18d4f4fa8e562a9b75ef711172cb0712be6a
Brandon <schwartzb4@mymacewan.ca>
2023-09-17 07:04:40 -0600

    update generate manifests to react 16, improve submission handling to prevent duplicate submissions

--- resources/assets/js/components/manifests/GenerateManifests.js
+++ resources/assets/js/components/manifests/GenerateManifests.js
@@ -1,127 +1,126 @@
-import React, {Component, createRef} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import {Button, Card, Col, InputGroup, Row} from 'react-bootstrap'
 import DatePicker from 'react-datepicker'
 import { ReactTabulator } from 'react-tabulator'
+import {useHistory} from 'react-router-dom'
 
-export default class GenerateManifests extends Component {
-    constructor() {
-        super()
-        this.state = {
-            employees: [],
-            startDate: new Date(),
-            endDate: new Date(),
-            tableRef: createRef()
-        }
-        this.handleChange = this.handleChange.bind(this)
-        this.refreshEmployees = this.refreshEmployees.bind(this)
-        this.store = this.store.bind(this)
-    }
+export default function GenerateManifests(props) {
+    const [employees, setEmployees] = useState([])
+    const [startDate, setStartDate] = useState(new Date((new Date()).getFullYear(), (new Date()).getMonth() - 1, 1))
+    const [endDate, setEndDate] = useState(new Date((new Date()).moveToFirstDayOfMonth().setHours(-1)))
+    const [isLoading, setIsLoading] = useState(true)
+    const [isStoring, setIsStoring] = useState(false)
 
-    componentDidMount() {
-        const currentDate = new Date()
-        const firstDayOfPreviousMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1)
-        //note the following line MODIFIES currentDate - so if you use it subsequently, beware!!
-        const lastDayOfPreviousMonth = new Date(currentDate.moveToFirstDayOfMonth().setHours(-1))
-        this.setState({startDate: firstDayOfPreviousMonth, endDate: lastDayOfPreviousMonth}, this.refreshEmployees)
-    }
+    const history = useHistory()
+    const tableRef = useRef()
 
-    handleChange(event) {
-        const {name, type, checked, value} = event.target
-        this.setState({[name]: type === 'checkbox' ? checked : value}, this.refreshEmployees)
-    }
+    useEffect(() => {
+        refreshEmployees()
+    }, [startDate, endDate])
 
-    refreshEmployees() {
+    const refreshEmployees = () => {
+        setIsLoading(true)
         const data = {
-            start_date: this.state.startDate.toLocaleString('en-US'),
-            end_date: this.state.endDate.toLocaleString('en-US')
+            start_date: startDate.toLocaleString('en-US'),
+            end_date: endDate.toLocaleString('en-US')
         }
+
         makeAjaxRequest('/manifests/getDriversToManifest', 'GET', data, result => {
-            this.setState({
-                employees: JSON.parse(result)
-            })
+            setEmployees(JSON.parse(result))
+            setIsLoading(false)
         })
     }
 
-    store() {
-        if(this.state.tableRef.current === undefined || this.state.tableRef.current.table.getSelectedData().length === 0) {
+    const store = () => {
+        if(isStoring)
+            return
+        else
+            setIsStoring(true)
+
+        if(tableRef.current === undefined || tableRef.current.table.getSelectedData().length === 0) {
             toastr.error('Please select at least one driver to manifest')
             return
         }
 
         const data = {
-            employees: this.state.tableRef.current.table.getSelectedData().map(employee => {return employee.employee_id}),
-            start_date: this.state.startDate.toLocaleString('en-US'),
-            end_date: this.state.endDate.toLocaleString('en-US')
+            employees: tableRef.current.table.getSelectedData().map(employee => {return employee.employee_id}),
+            start_date: startDate.toLocaleString('en-US'),
+            end_date: endDate.toLocaleString('en-US')
         }
         makeAjaxRequest('/manifests/store', 'POST', data, response => {
             toastr.clear()
             toastr.success('Successfully generated manifests', 'Success', {
                 'progressBar': true,
                 'showDuration': 500,
-                'onHidden': window.location = '/app/manifests',
+                'onHidden': () => history.push('/app/manifests'),
                 'positionClass': 'toast-top-center'
             })
+        }, error => {
+            setIsLoading(false)
         })
     }
 
-    render() {
-        const columns = [
-            {Title: 'Selected', field: 'isSelected', formatter: 'tickCross', hozAlign: 'center', headerHozAlign: 'center', headerSort: false, print: false, width: 50},
-            {title: 'Employee ID', field: 'employee_id'},
-            {title: 'Employee Number', field: 'employee_number'},
-            {title: 'Employee', field: 'label'},
-            {title: 'Valid Bills', field: 'valid_bill_count'},
-            {title: 'Legacy Bills', field: 'legacy_bill_count'},
-            {title: 'Incomplete Bills', field: 'incomplete_bill_count'}
-        ]
+    const columns = [
+        {Title: 'Selected', field: 'isSelected', formatter: 'tickCross', hozAlign: 'center', headerHozAlign: 'center', headerSort: false, print: false, width: 50},
+        {title: 'Employee ID', field: 'employee_id'},
+        {title: 'Employee Number', field: 'employee_number'},
+        {title: 'Employee', field: 'label'},
+        {title: 'Valid Bills', field: 'valid_bill_count'},
+        {title: 'Legacy Bills', field: 'legacy_bill_count'},
+        {title: 'Incomplete Bills', field: 'incomplete_bill_count'}
+    ]
 
-        return (
-            <Card>
-                <Card.Header>
+    return (
+        <Card>
+            <Card.Header>
+                <Row className='justify-content-md-center'>
+                    <h3>Generate Manifests</h3>
+                </Row>
+            </Card.Header>
+            <Card.Body>
+                <Row className='justify-content-md-center'>
+                    <Col md={3}>
+                        <InputGroup>
+                            <InputGroup.Text>Start Date: </InputGroup.Text>
+                            <DatePicker
+                                className='form-control'
+                                dateFormat='MMMM d, yyyy'
+                                placeholderText='After'
+                                selected={startDate}
+                                onChange={setStartDate}
+                                wrapperClassName='form-control'
+                            />
+                        </InputGroup>
+                    </Col>
+                    <Col md={3}>
+                        <InputGroup>
+                            <InputGroup.Text>End Date: </InputGroup.Text>
+                            <DatePicker
+                                className='form-control'
+                                dateFormat='MMMM d, yyyy'
+                                placeholderText='Before'
+                                selected={endDate}
+                                onChange={setEndDate}
+                                wrapperClassName='form-control'
+                            />
+                        </InputGroup>
+                    </Col>
+                    <Col md={2}>
+                        <Button variant='primary' disabled={!employees.length || isStoring} onClick={store}>Generate Manifests</Button>
+                    </Col>
+                </Row>
+            </Card.Body>
+            <Card.Footer>
+                {isLoading ? 
                     <Row className='justify-content-md-center'>
-                        <h3>Generate Manifests</h3>
-                    </Row>
-                </Card.Header>
-                <Card.Body>
-                    <Row className='justify-content-md-center'>
-                        <Col md={3}>
-                            <InputGroup>
-                                <InputGroup.Text>Start Date: </InputGroup.Text>
-                                <DatePicker
-                                    className='form-control'
-                                    dateFormat='MMMM d, yyyy'
-                                    placeholderText='After'
-                                    selected={this.state.startDate}
-                                    onChange={value => this.handleChange({target: {name: 'startDate', type: 'date', value: value}})}
-                                    wrapperClassName='form-control'
-                                />
-                            </InputGroup>
-                        </Col>
-                        <Col md={3}>
-                            <InputGroup>
-                                <InputGroup.Text>End Date: </InputGroup.Text>
-                                <DatePicker
-                                    className='form-control'
-                                    dateFormat='MMMM d, yyyy'
-                                    placeholderText='Before'
-                                    selected={this.state.endDate}
-                                    onChange={value => this.handleChange({target: {name: 'endDate', type: 'date', value: value}})}
-                                    wrapperClassName='form-control'
-                                />
-                            </InputGroup>
-                        </Col>
-                        <Col md={2}>
-                            <Button variant='primary' disabled={this.state.employees.length === 0} onClick={this.store}>Generate Manifests</Button>
-                        </Col>
-                    </Row>
-                </Card.Body>
-                <Card.Footer>
+                        <Col md={3}><h4><i className='fas fa-cog fa-spin'></i>  Loading...</h4></Col>
+                    </Row> :
                     <ReactTabulator
-                        ref={this.state.tableRef}
+                        ref={tableRef}
                         columns={columns}
-                        data={this.state.employees}
+                        data={employees}
                         dataLoaded={() => {
-                            const table = this.state.tableRef.current.table
+                            const table = tableRef.current.table
                             table.rowManager.rows.map(row => {
                                 const data = row.getData()
                                 if(data.valid_bill_count > 0 && data.incomplete_bill_count === 0 && data.legacy_bill_count === 0)
@@ -141,9 +140,9 @@ export default class GenerateManifests extends Component {
                             return selectable
                         }}
                     />
-                </Card.Footer>
-            </Card>
-        )
-    }
+                }
+            </Card.Footer>
+        </Card>
+    )
 }
 
