94640ea770b4ef66e20606f18b40604f6fdaa5ba
Brandon <schwartzb4@mymacewan.ca>
2023-11-07 14:40:17 -0700

    move logic to job from controller

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -10,19 +10,6 @@ use App\Jobs\ReceiveStripeWebhook;
 
 
 class WebhookController extends Controller {
-    private $ORDERED_PAYMENT_INTENT_STATUSES = [
-        NULL,
-        'payment_intent.pending', //custom status used when created, when there is no status from Stripe yet. As such receives lowest priority
-        'payment_intent.created',
-        'payment_intent.requires_payment_method',
-        'payment_intent.requires_confirmation',
-        'payment_intent.processing',
-        'payment_intent.succeded',
-        'payment_intent.requires_capture',
-        'payment_intent.requires_action',
-        'payment_intent.canceled',
-    ];
-
     public function ReceivePaymentIntentUpdate(Request $req) {
         try {
             $event = \Stripe\Webhook::constructEvent($req->getContent(), $req->header('Stripe-Signature'), config('services.stripe.webhook_secret'));
@@ -33,52 +20,7 @@ class WebhookController extends Controller {
         ReceiveStripeWebhook::dispatch($event);
 
         //Acknowledge that the request was received and successfully queued
-        return response()->json(['success' => 'Webhook received and queued', 200]);
-    }
-
-    public function ProcessPaymentIntentUpdate($event) {
-        activity('jobs')->log('Processing payment intent update for ' . $event->data->object->id);
-        $invoiceRepo = new Repos\InvoiceRepo();
-        $paymentRepo = new Repos\PaymentRepo();
-
-        DB::beginTransaction();
-
-        $paymentIntent = $event->data->object;
-        try {
-            $card = $paymentIntent->charges->data[0]->payment_method_details->card;
-        } catch (\Throwable $e) {
-            $card = null;
-        }
-
-        $stripePendingPaymentType = $paymentRepo->GetPaymentTypeByName("Stripe (Pending)");
-        $newPaymentType = $card ? $paymentRepo->GetPaymentTypeByName($card->brand) : $stripePendingPaymentType;
-        $payments = $paymentRepo->GetPaymentsByPaymentIntentId($paymentIntent->id);
-
-        foreach($payments as $payment) {
-            // only if we are 'upgrading' the status
-            // this means we don't process 'created' before 'success' but also means we never accidentally process the same payment twice. 
-            // which the stripe API makes a possibility. They do not guarantee idempotence, so instead this does
-            if(array_search($payment->payment_intent_status, $this->ORDERED_PAYMENT_INTENT_STATUSES) < array_search($event->type, $this->ORDERED_PAYMENT_INTENT_STATUSES)) {
-                $paymentRepo->UpdatePaymentIntentStatus($paymentIntent->id, $event->type);
-
-                if($payment->payment_type_id == $stripePendingPaymentType->payment_type_id && $newPaymentType) {
-                    $paymentRepo->Update($payment->payment_id, [
-                        'amount' => $payment->amount,
-                        'payment_type_id' => $newPaymentType->payment_type_id,
-                        'reference_value' => $card ? $card->last4 : null,
-                    ]);
-                }
-                if($event->type == 'payment_intent.succeeded')
-                    $invoiceRepo->AdjustBalanceOwing($payment->invoice_id, -$payment->amount);
-                if($event->type == 'payment_intent.cancelled')
-                    $invoiceRepo->AdjustBalanceOwing($payment->invoice_id, $payment->amount);
-            } else {
-                activity('[WebhookController]')
-                    ->log('[WebhookController.ProcessPaymentIntentUpdate] skipped. Previous payment intent status: ' . $payment->payment_intent_status . '. New payment intent status: ' . $event->type);
-            }
-        }
-
-        DB::commit();
+        return response()->json(['success' => 'Webhook received and queued'], 200);
     }
 }
 
--- app/Jobs/ReceiveStripeWebhook.php
+++ app/Jobs/ReceiveStripeWebhook.php
@@ -9,13 +9,26 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-use App\Http\Controllers\WebhookController;
+use App\Http\Repos;
 
 class ReceiveStripeWebhook implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     protected $event;
+    private $ORDERED_PAYMENT_INTENT_STATUSES = [
+        NULL,
+        'payment_intent.pending', //custom status used when created, when there is no status from Stripe yet. As such receives lowest priority
+        'payment_intent.created',
+        'payment_intent.requires_payment_method',
+        'payment_intent.requires_confirmation',
+        'payment_intent.processing',
+        'payment_intent.succeded',
+        'payment_intent.requires_capture',
+        'payment_intent.requires_action',
+        'payment_intent.canceled',
+    ];
+
     /**
      * Create a new job instance.
      *
@@ -34,8 +47,51 @@ class ReceiveStripeWebhook implements ShouldQueue
      */
     public function handle()
     {
-        activity('jobs')->log('Processing pending job for Stripe transaction ' . $this->event->data->object->id);
-        $controller = new WebhookController;
-        $controller->ProcessPaymentIntentUpdate($this->event);
+        activity('jobs')->log('Processing payment intent update for ' . $this->event->data->object->id);
+        $invoiceRepo = new Repos\InvoiceRepo();
+        $paymentRepo = new Repos\PaymentRepo();
+
+        DB::beginTransaction();
+
+        $paymentIntent = $this->event->data->object;
+        try {
+            $card = $paymentIntent->charges->data[0]->payment_method_details->card;
+        } catch (\Throwable $e) {
+            $card = null;
+        }
+        activity('jobs')->log('Card type: ' . $card);
+
+        $stripePendingPaymentType = $paymentRepo->GetPaymentTypeByName("Stripe (Pending)");
+        $newPaymentType = $card ? $paymentRepo->GetPaymentTypeByName($card->brand) : $stripePendingPaymentType;
+        $payments = $paymentRepo->GetPaymentsByPaymentIntentId($paymentIntent->id);
+
+        foreach($payments as $payment) {
+            activity('jobs')->log('Payment: ' . $payment->payment_id . '. Payment Intent: ' . $payment->payment_intent_id);
+            activity('jobs')->log('Previous status: ' . $payment->payment_intent_status . '. New status: ' . $event->status); 
+            activity('jobs')->log('Previous status: ' . array_search($payment->payment_intent_status, $this->ORDERED_PAYMENT_INTENT_STATUSES) . '. New status: ' . array_search($event->status, $this->ORDERED_PAYMENT_INTENT_STATUSES));
+            // only if we are 'upgrading' the status
+            // this means we don't process 'created' before 'success' but also means we never accidentally process the same payment twice. 
+            // which the stripe API makes a possibility. They do not guarantee idempotence, so instead this does
+            if(array_search($payment->payment_intent_status, $this->ORDERED_PAYMENT_INTENT_STATUSES) < array_search($this->event->type, $this->ORDERED_PAYMENT_INTENT_STATUSES)) {
+                $paymentRepo->UpdatePaymentIntentStatus($paymentIntent->id, $this->event->type);
+
+                if($payment->payment_type_id == $stripePendingPaymentType->payment_type_id && $newPaymentType) {
+                    $paymentRepo->Update($payment->payment_id, [
+                        'amount' => $payment->amount,
+                        'payment_type_id' => $newPaymentType->payment_type_id,
+                        'reference_value' => $card ? $card->last4 : null,
+                    ]);
+                }
+                if($this->event->type == 'payment_intent.succeeded')
+                    $invoiceRepo->AdjustBalanceOwing($payment->invoice_id, -$payment->amount);
+                if($this->event->type == 'payment_intent.cancelled')
+                    $invoiceRepo->AdjustBalanceOwing($payment->invoice_id, $payment->amount);
+            } else {
+                activity('jobs')
+                    ->log('[WebhookController.ProcessPaymentIntentUpdate] skipped. Previous payment intent status: ' . $payment->payment_intent_status . '. New payment intent status: ' . $event->type);
+            }
+        }
+
+        DB::commit();
     }
 }
