834dcd92c0662c3e4e82fa589d3414902c635599
Brandon <schwartzb4@mymacewan.ca>
2023-11-07 11:49:37 -0700

    fix typo in webhook handling job

--- app/Jobs/ReceiveStripeWebhook.php
+++ app/Jobs/ReceiveStripeWebhook.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-use App\Htpp\Controllers\WebhookController;
+use App\Http\Controllers\WebhookController;
 
 class ReceiveStripeWebhook implements ShouldQueue
 {
