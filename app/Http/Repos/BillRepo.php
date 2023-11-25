d6c8475cf0995fafe8e4d58b34015b6e6965730c
Brandon <schwartzb4@mymacewan.ca>
2023-11-24 19:11:20 -0700

    remove accounts_payable binary check

--- app/Http/Repos/BillRepo.php
+++ app/Http/Repos/BillRepo.php
@@ -143,7 +143,7 @@ class BillRepo {
         $bills = LineItem::leftJoin('charges', 'charges.charge_id', '=', 'line_items.charge_id')
             ->leftJoin('bills', 'bills.bill_id', '=', 'charges.bill_id')
             ->leftjoin('payment_types', 'payment_types.payment_type_id', '=', 'charges.charge_type_id')
-            ->where('payment_types.is_account_payable', 1)
+            ->where('payment_types.type', 'accounts_payable')
             ->where('time_pickup_scheduled', '>=', $startDate->format('Y-m-01'))
             ->where('time_pickup_scheduled', '<=', $endDate->format('Y-m-t'))
             ->where(function ($query) {
@@ -467,7 +467,7 @@ class BillRepo {
             ->join('addresses as delivery', 'delivery.address_id', '=', 'bills.delivery_address_id')
             ->join('selections', 'selections.value', '=', 'bills.delivery_type')
             ->where('amendment_number', null)
-            ->where('payment_types.is_account_payable', 0)
+            ->where('payment_types.type', '!=', 'accounts_payable')
             ->select(
                 'payment_types.payment_type_id as account_id',
                 'payment_types.name as account_name',
@@ -544,7 +544,6 @@ class BillRepo {
             ->where('payment_types.type', 'prepaid')
             ->where('time_pickup_scheduled', '>=', $startDate->format('Y-m-01'))
             ->where('time_pickup_scheduled', '<=', $endDate->format('Y-m-t'))
-            ->where('payment_types.is_account_payable', 0)
             ->select(
                 'payment_types.payment_type_id',
                 'payment_types.name as payment_type_name',
