7968afded0f78f028d18b799d7b77bdc37965d1d
Brandon <schwartzb4@mymacewan.ca>
2024-01-15 20:56:04 -0700

    fix boolean table filter clearing when value is false

--- resources/assets/js/components/tableFilters/BooleanFilter.js
+++ resources/assets/js/components/tableFilters/BooleanFilter.js
@@ -5,10 +5,14 @@ export default function BooleanFilter(props) {
     const [boolState, setBoolState] = useState(false)
 
     useEffect(() => {
-        if(props.filter.value != undefined)
-            setBoolState(props.filter.value == true || props.filter.value == 'true')
-        else if (props.filter.default)
+        if(props.filter.value != undefined) {
+            const trueOrFalse = props.filter.value == true || props.filter.value == 'true'
+            setBoolState(trueOrFalse)
+            props.handleFilterValueChange({...props.filter, value: trueOrFalse.toString()})
+        } else if (props.filter.default) {
             setBoolState(props.filter.default)
+            props.handleFilterValueChange({...props.filter, value: props.filter.default.toString()})
+        }
     }, [props.filter.value])
 
     const setFilterValue = (newBoolState) => {
