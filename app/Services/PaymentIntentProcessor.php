8d77a511fd2adbb178783e57df76b57d8bd8251f
Brandon <schwartzb4@mymacewan.ca>
2024-02-01 16:01:57 -0700

    do string replacement on payment intent processor instead

--- app/Services/PaymentIntentProcessor.php
+++ app/Services/PaymentIntentProcessor.php
@@ -42,25 +42,24 @@ class PaymentIntentProcessor {
             // only if we are 'upgrading' the status
             // this means we don't process 'created' before 'success' but also means we never accidentally process the same payment twice. 
             // which the stripe API makes a possibility. They do not guarantee idempotence, so instead this does
-            $oldStatus = explode('.', $payment->payment_intent_status);
-            $oldStatus = end($oldStatus);
+            $oldStatus = str_replace('payment_intent.', '', $payment->payment_intent_status);
             $oldStatusIndex = array_search($oldStatus, $this->ORDERED_PAYMENT_INTENT_STATUSES);
-            $newStatus = explode('.', $event->type);
-            $newStatus = end($newStatus);
+            $newState = str_replace('payment_intent.', '', $event->type);
             $newStatusIndex = array_search($newStatus, $this->ORDERED_PAYMENT_INTENT_STATUSES);
+
             if($oldStatusIndex && $newStatusIndex && $oldStatusIndex < $newStatusIndex) {
                 $paymentRepo->UpdatePaymentIntentStatus($paymentIntent->id, $newStatus);
 
                 $paymentRepo->Update($payment->payment_id, [
-                    'amount' => $payment->amount,
+                    'amount' => $payment->amount_received,
                     'payment_type_id' => $paymentType->payment_type_id,
                     'reference_value' => $card ? '**** **** **** ' . $card->last4 : null,
                 ]);
 
                 if($newStatus == 'succeeded')
-                    $invoiceRepo->AdjustBalanceOwing($payment->invoice_id, -(Decimal($payment->amount) / Decimal(100)));
+                    $invoiceRepo->AdjustBalanceOwing($payment->invoice_id, -(Decimal($payment->amount_received) / Decimal(100)));
                 if($newStatus == 'canceled')
-                    $invoiceRepo->AdjustBalanceOwing($payment->invoice_id, Decimal($payment->amount) / Decimal(100));
+                    $invoiceRepo->AdjustBalanceOwing($payment->invoice_id, Decimal($payment->amount_received) / Decimal(100));
             } else {
                 activity('jobs')
                     ->log('[ReceiveStripeWebhook.handle] skipped. Previous payment intent status: ' . $payment->payment_intent_status . '. New payment intent status: ' . $event->type);
