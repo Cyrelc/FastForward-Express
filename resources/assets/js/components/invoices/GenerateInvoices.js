0243fb5004008537d6e725858cc4af46768428ee
Brandon <schwartzb4@mymacewan.ca>
2024-02-05 20:15:30 -0700

    update GenerateInvoices to use Tabulator directly

--- resources/assets/js/components/invoices/GenerateInvoices.js
+++ resources/assets/js/components/invoices/GenerateInvoices.js
@@ -2,81 +2,17 @@ import React, {useEffect, useRef, useState} from 'react'
 import {Button, Card, Col, InputGroup, Row} from 'react-bootstrap'
 import Select from 'react-select'
 import DatePicker from 'react-datepicker'
-import {ReactTabulator} from 'react-tabulator'
+import {TabulatorFull as Tabulator} from 'tabulator-tables'
 
 export default function GenerateInvoices(props) {
     const [invoiceIntervals, setInvoiceIntervals] = useState([])
     const [startDate, setStartDate] = useState()
     const [endDate, setEndDate] = useState()
-    const [selectedInvoiceIntervals, setSelectedInvoiceIntervals] = useState([])
     const [pendingCreation, setPendingCreation] = useState([])
+    const [selectedInvoiceIntervals, setSelectedInvoiceIntervals] = useState([])
+    const [table, setTable] = useState(null)
 
-    const tableRef = useRef()
-
-    useEffect(() => {
-        document.title = 'Generate Invoices - Fast Forward Express'
-        makeAjaxRequest('/invoices/getModel', 'GET', null, response => {
-            response = JSON.parse(response)
-            setInvoiceIntervals(response.invoice_intervals)
-            setStartDate(Date.parse(response.start_date))
-            setEndDate(Date.parse(response.end_date))
-        })
-    }, [])
-
-    useEffect(() => {
-        if(selectedInvoiceIntervals === null || selectedInvoiceIntervals.length === 0 || !startDate || !endDate) {
-            setPendingCreation([])
-            return
-        }
-
-        const data = {
-            invoice_intervals: selectedInvoiceIntervals.filter(interval => interval.type == 'invoice_interval').map(interval => {return interval.value}),
-            prepaid_types: selectedInvoiceIntervals.filter(interval => interval.type == 'prepaid_type').map(interval => {return interval.value}),
-            start_date: startDate.toLocaleString('en-US'),
-            end_date: endDate.toLocaleString('en-US')
-        }
-
-        makeAjaxRequest('/invoices/getUninvoiced', 'POST', data, response => {
-            response = JSON.parse(response)
-            toastr.clear()
-            if(response.pending_creation)
-                setPendingCreation(Object.values(response.pending_creation))
-        })
-    }, [startDate, endDate, selectedInvoiceIntervals])
-
-    const selectAll = () => {
-        let selected = []
-        invoiceIntervals.forEach(group => {
-            group.options.forEach(option => selected.push(option))
-        })
-        setSelectedInvoiceIntervals(selected)
-    }
-
-    const store = () => {
-        if(tableRef.current?.table?.getSelectedData().length === 0) {
-            toastr.error('Please select at least one invoice to be created')
-            return
-        }
-
-        const selected = tableRef.current.table.getSelectedData()
-
-        const data = {
-            accounts: selected.filter(row => row.type == 'account').map(account => account.id),
-            prepaid: selected.filter(row => row.type == 'prepaid').map(bill => bill.charge_id),
-            start_date: startDate.toLocaleString('en-US'),
-            end_date: endDate.toLocaleString('en-US')
-        }
-
-        makeAjaxRequest('/invoices', 'POST', data, response => {
-            toastr.clear()
-            toastr.success('Successfully generated invoices', 'Success', {
-                'progressBar' : true,
-                'showDuration': 500,
-                'onHidden': window.location = '/app/invoices',
-                'positionClass': 'toast-top-center'
-            })
-        })
-    }
+    const tableRef = useRef(null)
 
     const columns = [
         {
@@ -160,6 +96,114 @@ export default function GenerateInvoices(props) {
         {title: 'group', field: 'group', visible: false}
     ]
 
+    useEffect(() => {
+        document.title = 'Generate Invoices - Fast Forward Express'
+        makeAjaxRequest('/invoices/getModel', 'GET', null, response => {
+            response = JSON.parse(response)
+            setInvoiceIntervals(response.invoice_intervals)
+            setStartDate(Date.parse(response.start_date))
+            setEndDate(Date.parse(response.end_date))
+        })
+    }, [])
+
+    useEffect(() => {
+        if(tableRef.current && !table) {
+            const newTabulator = new Tabulator(tableRef.current, {
+                placeholder: 'No accounts or charges fit the selected criteria for invoicing',
+                columns: columns,
+                data: pendingCreation,
+                initialSort: [{column: 'number', dir: 'asc'}],
+                groupBy: 'parent_account',
+                layout: 'fitColumns',
+                maxHeight: '65vh',
+                selectable: true,
+                selectableCheck: row => {
+                    const selectable = row.getData().valid_bill_count > 0
+                    return selectable
+                },
+            })
+
+            newTabulator.on('rowDeselected', row => {
+                row.update({isSelected: false})
+            })
+
+            newTabulator.on('rowSelected', row => {
+                row.update({isSelected: true})
+            })
+
+            setTable(newTabulator)
+        }
+    }, [tableRef.current])
+
+    // Handle table data changes
+    useEffect(() => {
+        if(table)
+            table.setData(pendingCreation).then(() => {
+                table.getRows().map(row => {
+                    const data = row.getData()
+                    if(data.valid_bill_count > 0 && data.incomplete_bill_count === 0 && data.skipped_bill_count === 0 && data.legacy_bill_count === 0) {
+                        row.select()
+                        row.update({isSelected: true})
+                    }
+                })
+            })
+    }, [pendingCreation])
+
+    useEffect(() => {
+        if(selectedInvoiceIntervals === null || selectedInvoiceIntervals.length === 0 || !startDate || !endDate) {
+            setPendingCreation([])
+            return
+        }
+
+        const data = {
+            invoice_intervals: selectedInvoiceIntervals.filter(interval => interval.type == 'invoice_interval').map(interval => {return interval.value}),
+            prepaid_types: selectedInvoiceIntervals.filter(interval => interval.type == 'prepaid_type').map(interval => {return interval.value}),
+            start_date: startDate.toLocaleString('en-US'),
+            end_date: endDate.toLocaleString('en-US')
+        }
+
+        makeAjaxRequest('/invoices/getUninvoiced', 'POST', data, response => {
+            response = JSON.parse(response)
+            toastr.clear()
+            if(response.pending_creation)
+                setPendingCreation(Object.values(response.pending_creation))
+        })
+    }, [startDate, endDate, selectedInvoiceIntervals])
+
+    const selectAll = () => {
+        let selected = []
+        invoiceIntervals.forEach(group => {
+            group.options.forEach(option => selected.push(option))
+        })
+        setSelectedInvoiceIntervals(selected)
+    }
+
+    const store = () => {
+        if(table?.getSelectedData().length === 0) {
+            toastr.error('Please select at least one invoice to be created')
+            return
+        }
+
+        const selected = table.getSelectedData()
+
+        const data = {
+            accounts: selected.filter(row => row.type == 'account').map(account => account.id),
+            prepaid: selected.filter(row => row.type == 'prepaid').map(bill => bill.charge_id),
+            start_date: startDate.toLocaleString('en-US'),
+            end_date: endDate.toLocaleString('en-US')
+        }
+
+        makeAjaxRequest('/invoices', 'POST', data, response => {
+            toastr.clear()
+            toastr.success('Successfully generated invoices', 'Success', {
+                'progressBar' : true,
+                'showDuration': 500,
+                'onHidden': window.location = '/app/invoices',
+                'positionClass': 'toast-top-center'
+            })
+        })
+    }
+
     function isValidForInvoicing(cell) {
         const data = cell.getData()
         if(data.valid_bill_count === 0) {
@@ -232,40 +276,7 @@ export default function GenerateInvoices(props) {
             </Card.Body>
             <Card.Footer>
                 <p>The following accounts and/or charges fit the selected criteria and have yet to be invoiced:</p>
-                <ReactTabulator
-                    placeholder='No accounts or charges fit the selected criteria for invoicing'
-                    ref={tableRef}
-                    columns={columns}
-                    data={pendingCreation}
-                    dataLoaded={() => {
-                        const table = tableRef.current.table
-                        table.rowManager.rows.map(row => {
-                            const data = row.getData()
-                            if(data.valid_bill_count > 0 && data.incomplete_bill_count === 0 && data.skipped_bill_count === 0 && data.legacy_bill_count === 0) {
-                                table.selectRow(row)
-                            }
-                        })
-                    }}
-                    initialSort={[{column: 'account_number', dir: 'asc'}]}
-                    options={{
-                        groupBy: 'parent_account',
-                        layout: 'fitColumns',
-                        maxHeight: '65vh',
-                        rowDeselected: row => {
-                            console.log('rowDeselected')
-                            row.update({isSelected: false})
-                        },
-                        rowSelected: row => {
-                            console.log('rowSelected')
-                            row.update({isSelected: true})
-                        },
-                        selectable: true,
-                        selectableCheck: row => {
-                            const selectable = row.getData().valid_bill_count > 0
-                            return selectable
-                        }
-                    }}
-                />
+                <div ref={tableRef}></div>
             </Card.Footer>
         </Card>
     )
