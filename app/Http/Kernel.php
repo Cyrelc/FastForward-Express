a7267a1b5c47c3354399d26fa6b062c9b5cb86c0
Brandon <schwartzb4@mymacewan.ca>
2024-02-13 11:05:02 -0700

    add TrimStrings and ConvertEmptyStringsToNull laravel middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,6 +28,8 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
+            \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \App\Http\Middleware\SetImpersonationCauser::class,
         ],
