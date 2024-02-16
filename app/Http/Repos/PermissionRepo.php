83fd76e890da52f9e39857d452970b8c57565d89
Brandon <schwartzb4@mymacewan.ca>
2024-02-16 09:04:34 -0700

    explicitely check truthful state on permissions

--- app/Http/Repos/PermissionRepo.php
+++ app/Http/Repos/PermissionRepo.php
@@ -7,16 +7,15 @@ use Spatie\Permission\Models\Role;
 use App\User;
 
 class PermissionRepo {
-
     public function assignUserPermissions($user, $permissions) {
         $permissionsChanged = [];
 
         foreach($permissions as $name => $value) {
-            if($user->can($name) && !$value) {
+            if($user->can($name) && $value == false) {
                 $permissionsChanged['attributes'][$name] = 'false';
                 $permissionsChanged['old'][$name] = 'true';
                 $user->revokePermissionTo($name);
-            } elseif($user->cannot($name) && $value) {
+            } elseif($user->cannot($name) && $value == true) {
                 $permissionsChanged['attributes'][$name] = 'true';
                 $permissionsChanged['old'] = 'false';
                 $user->givePermissionTo($name);
