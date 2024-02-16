e93bfb1177f918a98e43992d1ba804b7f15e0b52
Brandon <schwartzb4@mymacewan.ca>
2024-02-16 16:05:01 -0700

    update userCollector to use email_addresses

--- app/Http/Collectors/UserCollector.php
+++ app/Http/Collectors/UserCollector.php
@@ -5,17 +5,18 @@ namespace App\Http\Collectors;
 use App\Http\Repos;
 
 class UserCollector {
-    public function CollectUserForEmployee($req) {
-        $userId = null;
-        foreach($req->emails as $email)
+    public function collect($req, $userId = null) {
+        foreach($req->email_addresses as $email)
         if(filter_var($email['is_primary'], FILTER_VALIDATE_BOOLEAN)) {
             $primaryEmail = $email['email'];
             break;
         }
+        if(!$primaryEmail)
+
 
         if(isset($req->employee_id) && $req->employee_id !== '') {
             $employeeRepo = new Repos\EmployeeRepo();
-            $userId = $employeeRepo->GetById($req->employee_id, null)->user_id;
+            $userId = $employeeRepo->getById($req->employee_id, null)->user_id;
         }
 
         $user = [
@@ -28,130 +29,7 @@ class UserCollector {
         return $user;
     }
 
-    public function CollectAccountUser($req, $contactId, $primaryEmail, $userId = null) {
-        $accountRepo = new Repos\AccountRepo();
-        $account = $accountRepo->GetById($req->account_id);
-
-        $accountUser = [
-            'account_id' => $req->account_id,
-            'contact_id' => $contactId,
-            'user_id' => $userId,
-            'email' => $primaryEmail
-        ];
-
-        if($req->user()->can('updateAccountUserPermissions', $account))
-            $accountUser = array_merge($accountUser, ['is_enabled' => filter_var($req->permissions['is_enabled'], FILTER_VALIDATE_BOOLEAN)]);
-
-        return $accountUser;
-    }
-
-    public function CollectAccountUserPermissions($req) {
-        $permissionMap = [
-            /* Accounts */
-            'accounts.edit.basic.my' => 'editAccountBasicMy',
-            'accounts.edit.basic.children' => 'editAccountBasicChildren',
-            'accounts.edit.invoicing.my' => 'editAccountInvoiceSettingsMy',
-            'accounts.edit.invoicing.children' => 'editAccountInvoiceSettingsChildren',
-            'accounts.view.basic.children' => 'viewAccountsBasicChildren',
-            'accounts.view.activityLog.my' => 'viewAccountActivityLogMy',
-            'accounts.view.activityLog.children' => 'viewAccountActivityLogChildren',
-            /* Account Users */
-            'accountUsers.create.my' => 'createAccountUsersMy',
-            'accountUsers.create.children' => 'createAccountUsersChildren',
-            'accountUsers.edit.basic.my' => 'editAccountUsersMy',
-            'accountUsers.edit.basic.children' => 'editAccountUsersChildren',
-            'accountUsers.edit.permissions.my' => 'editAccountUserPermissionsMy',
-            'accountUsers.edit.permissions.children' => 'editAccountUserPermissionsChildren',
-            'accountUsers.view.activityLog.children' => 'viewAccountUserActivityLogsChildren',
-            'accountUsers.view.activityLog.my' => 'viewAccountUserActivityLogsMy',
-            'accountUsers.view.permissions.children' => 'viewAccountUserPermissionsChildren',
-            'accountUsers.view.permissions.my' => 'viewAccountUserPermissionsMy',
-            /* Bills */
-            'bills.create.basic.my' => 'createBillsMy',
-            'bills.create.basic.children' => 'createBillsChildren',
-            'bills.view.basic.my' => 'viewBillsMy',
-            'bills.view.basic.children' => 'viewBillsChildren',
-            /* Invoices */
-            'invoices.view.my' => 'viewInvoicesMy',
-            'invoices.view.children' => 'viewInvoicesChildren',
-            /* Payments */
-            'payments.edit.children' => 'editPaymentsChildren',
-            'payments.edit.my' => 'editPaymentsMy',
-            'payments.view.children' => 'viewPaymentsChildren',
-            'payments.view.my' => 'viewPaymentsMy',
-        ];
-
-        $permissions = [];
-        foreach($permissionMap as $key => $value)
-            $permissions[$key] = filter_var($req->permissions[$value], FILTER_VALIDATE_BOOLEAN);
-
-        return $permissions;
-    }
-
-    public function CollectEmployeePermissions($req) {
-        $permissionMap = [
-            /* Accounts */
-            'accounts.create' => 'createAccounts',
-            'accounts.edit.basic.*' => 'editAccountsBasic',
-            'accounts.edit.*.*' => 'editAccountsFull',
-            'accounts.view.basic.*' => 'viewAccountsBasic',
-            'accounts.view.*.*' => 'viewAccountsFull',
-            /* Account Users */
-            'accountUsers.create.*.*' => 'createAccountUsers',
-            'accountUsers.delete.*.*' => 'deleteAccountUsers',
-            'accountUsers.edit.*.*' => 'editAccountUsers',
-            'accountUsers.impersonate.*' => 'impersonateAccountUsers',
-            /* App Settings */
-            'appSettings.edit.*.*' => 'editAppSettings',
-            /* Bills */
-            'bills.create.basic.*' => 'createBillsBasic',
-            'bills.create.*.*' => 'createBillsFull',
-            'bills.delete' => 'deleteBills',
-            'bills.view.basic.*' => 'viewBillsBasic',
-            'bills.view.dispatch.*' => 'viewBillsDispatch',
-            'bills.view.billing.*' => 'viewBillsBilling',
-            'bills.view.activityLog.*' => 'viewBillsActivityLog',
-            'bills.edit.basic.*' => 'editBillsBasic',
-            'bills.edit.dispatch.*' => 'editBillsDispatch',
-            'bills.edit.billing.*' => 'editBillsBilling',
-            /* Chargebacks */
-            'chargebacks.view.*.*' => 'viewChargebacks',
-            'chargebacks.edit.*.*' => 'editChargebacks',
-            /* Employees */
-            'employees.create' => 'createEmployees',
-            'employees.view.basic.*' => 'viewEmployeesBasic',
-            'employees.view.*.*' => 'viewEmployeesAdvanced',
-            'employees.edit.basic.*' => 'editEmployeesBasic',
-            'employees.edit.*.*' => 'editEmployeesAdvanced',
-            /* Invoices */
-            'invoices.create' => 'createInvoices',
-            'invoices.view.*.*' => 'viewInvoices',
-            'invoices.edit.*.*' => 'editInvoices',
-            'invoices.delete' => 'deleteInvoices',
-            /* Manifests */
-            'manifests.create.*.*' => 'createManifests',
-            'manifests.delete' => 'deleteManifests',
-            'manifests.edit.*.*' => 'editManifests',
-            'manifests.view.*.*' => 'viewManifests',
-            /* Payments */
-            'payments.create.*.*' => 'createPayments',
-            'payments.edit.*' => 'editPayments',
-            'payments.delete.*.*' => 'revertPayments',
-            'payments.view.*.*' => 'viewPayments',
-            /* Ratesheets */
-            'ratesheets.create.*.*' => 'editAppSettings',
-            'ratesheets.edit.*.*' => 'editAppSettings',
-            'ratesheets.view.*.*' => 'editAppSettings'
-        ];
-
-        $permissions = [];
-        foreach($permissionMap as $key => $value)
-            $permissions[$key] = filter_var($req->permissions[$value], FILTER_VALIDATE_BOOLEAN);
-
-        return $permissions;
-    }
-
-    public function CollectSettings($req) {
+    public function collectSettings($req) {
         return [
             'use_imperial_default' => filter_var($req->use_imperial_default, FILTER_VALIDATE_BOOLEAN)
         ];
