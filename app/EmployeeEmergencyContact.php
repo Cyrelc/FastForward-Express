ff027b7e0ca76e41d93f58cf214602ec5d0fdc6e
Brandon <schwartzb4@mymacewan.ca>
2024-02-16 09:04:03 -0700

    add relationships and factory to emergency contacts

--- app/EmployeeEmergencyContact.php
+++ app/EmployeeEmergencyContact.php
@@ -3,17 +3,26 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Activitylog\LogOptions;
 
 class EmployeeEmergencyContact extends Model
 {
-    use LogsActivity;
+    use HasFactory, LogsActivity;
 
     public $primaryKey = 'contact_id';
     public $timestamps = false;
 
     protected $fillable = ['employee_id', 'contact_id', 'is_primary'];
+
+    public function contact() {
+        return $this->hasOne(\App\Models\Contact::class, 'contact_id');
+    }
+
+    public function employee() {
+        return $this->hasOne('employees', 'employee_id');
+    }
 
     public function getActivityLogOptions() : LogOptions {
         return LogOptions::defaults()
