5fe4080ec9751f53a3a16e99be88181279f543a9
Brandon <schwartzb4@mymacewan.ca>
2024-02-19 18:18:09 -0700

    delete obsolete version of EmailAddress model

--- database/factories/EmailAddressFactory.php
+++ database/factories/EmailAddressFactory.php
@@ -6,7 +6,7 @@ use App\Models\EmailAddress;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\EmailAddress>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\EmailAddress>
  */
 class EmailAddressFactory extends Factory
 {
