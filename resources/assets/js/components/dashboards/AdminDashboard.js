6fceb994c685c923c0501ca652d8fcd7bce0ea94
Brandon <schwartzb4@mymacewan.ca>
2024-03-21 13:45:11 -0600

    fix nav link for employee expiry columns

--- resources/assets/js/components/dashboards/AdminDashboard.js
+++ resources/assets/js/components/dashboards/AdminDashboard.js
@@ -44,7 +44,7 @@ export default function AdminDashboard(props) {
     ]
 
     const employeeExpiryColumns = [
-        {title: 'Employee', field: 'employee_id', formatter: 'link', formatterParams: {labelField: 'employee_name', urlPrefix: '/employees/edit/'}},
+        {title: 'Employee', field: 'employee_id', formatter: 'link', formatterParams: {labelField: 'employee_name', urlPrefix: '/app/employees/'}},
         {title: 'Date', field: 'date', formatter: cell => {
             const date = Date.parse(cell.getValue())
             const today = new Date()
