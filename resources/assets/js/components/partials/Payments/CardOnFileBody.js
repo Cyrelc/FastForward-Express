d8a3e2cfb5e236b6e88b41d51e05f6af8f8b7588
Brandon <schwartzb4@mymacewan.ca>
2024-05-02 11:32:52 -0600

    add missing api import

--- resources/assets/js/components/partials/Payments/CardOnFileBody.js
+++ resources/assets/js/components/partials/Payments/CardOnFileBody.js
@@ -1,8 +1,11 @@
 import React, {Fragment, useState} from 'react'
 import {Button, ButtonGroup, Col, FormControl, Modal, Row} from 'react-bootstrap'
+import {useAPI} from '../../../contexts/APIContext'
 
 export default function CardOnFileBody(props) {
     const [comment, setComment] = useState('')
+
+    const api = useApi()
 
     const storePayment = () => {
         const data = {
