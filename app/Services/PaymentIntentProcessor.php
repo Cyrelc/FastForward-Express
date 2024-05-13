a92543089c3b5d56b7648d1d8bfa95dfa256d52e
Brandon <schwartzb4@mymacewan.ca>
2024-05-13 13:31:21 -0600

    add try catch to payment intent processing

--- app/Services/PaymentIntentProcessor.php
+++ app/Services/PaymentIntentProcessor.php
@@ -42,42 +42,50 @@ class PaymentIntentProcessor {
             // only if we are 'upgrading' the status
             // this means we don't process 'created' before 'success' but also means we never accidentally process the same payment twice. 
             // which the stripe API makes a possibility. They do not guarantee idempotence, so instead this does
-            $oldStatus = str_replace('payment_intent.', '', $payment->payment_intent_status);
-            $oldStatusIndex = array_search($oldStatus, $this->ORDERED_PAYMENT_INTENT_STATUSES);
-            $newStatus = str_replace('payment_intent.', '', $event->data->status);
-            $newStatusIndex = array_search($newStatus, $this->ORDERED_PAYMENT_INTENT_STATUSES);
+            try {
+                $oldStatus = str_replace('payment_intent.', '', $payment->payment_intent_status);
+                $oldStatusIndex = array_search($oldStatus, $this->ORDERED_PAYMENT_INTENT_STATUSES);
+                $newStatus = str_replace('payment_intent.', '', $event->data->status);
+                $newStatusIndex = array_search($newStatus, $this->ORDERED_PAYMENT_INTENT_STATUSES);
 
-            if($oldStatusIndex == false || $newStatusIndex == false) {
-                activity('payment_intent')
-                    ->performedOn($payment)
-                    ->withProperties(['new_status_index' => $newStatusIndex, 'old_status_index' => $oldStatusIndex, 'new_status' => $event->data->status, 'old_status' => $payment->payment_intent_status])
-                    ->log(['[ReceiveStripeWebhook.handle] invalid status found']);
-            } else if($newStatusIndex > $oldStatusIndex) {
-                $paymentAmount = bcdiv($paymentIntent->amount_received, 100, 2);
-
-                $payment->update([
-                    'amount' => $paymentAmount,
-                    'error' => $paymentIntent->last_payment_error ? $paymentIntent->last_payment_error->message : null,
-                    'payment_intent_status' => $newStatus,
-                    'payment_type_id' => $paymentType->payment_type_id,
-                    'receipt_url' => $paymentIntent->charges->data[0]->receipt_url ?? null,
-                    'reference_value' => $card ? '**** **** **** ' . $card->last4 : null,
-                ]);
-
-                if($newStatus == 'succeeded') {
+                if($oldStatusIndex == false || $newStatusIndex == false) {
                     activity('payment_intent')
                         ->performedOn($payment)
-                        ->withProperties(['payment_intent_id' => $paymentIntent->id, 'webhook_status' => $event->data->status, 'amount' => $paymentAmount, 'receipt_url' => $paymentIntent->charges->data[0]->receipt_url])
-                        ->log('[ReceiveStripeWebhook.handle] succeeded');
-                    if($invoice->balance_owing == 0)
-                        throw new \Exception('Attempting to double pay invoice #' . $payment->invoice_id, $event);
-                    $invoiceRepo->AdjustBalanceOwing($payment->invoice_id, -$paymentAmount);
+                        ->withProperties(['new_status_index' => $newStatusIndex, 'old_status_index' => $oldStatusIndex, 'new_status' => $event->data->status, 'old_status' => $payment->payment_intent_status])
+                        ->log(['[ReceiveStripeWebhook.handle] invalid status found']);
+                } else if($newStatusIndex > $oldStatusIndex) {
+                    $paymentAmount = bcdiv($paymentIntent->amount_received, 100, 2);
+
+                    $payment->update([
+                        'amount' => $paymentAmount,
+                        'error' => $paymentIntent->last_payment_error ? $paymentIntent->last_payment_error->message : null,
+                        'payment_intent_status' => $newStatus,
+                        'payment_type_id' => $paymentType->payment_type_id,
+                        'receipt_url' => $paymentIntent->charges->data[0]->receipt_url ?? null,
+                        'reference_value' => $card ? '**** **** **** ' . $card->last4 : null,
+                    ]);
+
+                    if($newStatus == 'succeeded') {
+                        activity('payment_intent')
+                            ->performedOn($payment)
+                            ->withProperties(['payment_intent_id' => $paymentIntent->id, 'webhook_status' => $event->data->status, 'amount' => $paymentAmount, 'receipt_url' => $paymentIntent->charges->data[0]->receipt_url])
+                            ->log('[ReceiveStripeWebhook.handle] succeeded');
+                        if($invoice->balance_owing == 0)
+                            throw new \Exception('Attempting to double pay invoice #' . $payment->invoice_id, $event);
+                        $invoiceRepo->AdjustBalanceOwing($payment->invoice_id, -$paymentAmount);
+                    }
+                } else {
+                    activity('jobs')
+                        ->performedOn($payment)
+                        ->withProperties(['payment_intent_id' => $paymentIntent->id, 'database_status' => $payment->payment_intent_status, 'webhook_status' => $event->data->status])
+                        ->log('[ReceiveStripeWebhook.handle] skipped.');
                 }
-            } else {
+            } catch (\Throwable $e) {
                 activity('jobs')
+                    ->event('error')
                     ->performedOn($payment)
-                    ->withProperties(['payment_intent_id' => $paymentIntent->id, 'database_status' => $payment->payment_intent_status, 'webhook_status' => $event->data->status])
-                    ->log('[ReceiveStripeWebhook.handle] skipped.');
+                    ->withProperties(['payment_intent_event' => $event])
+                    ->log('[ReceiveStripeWebhook.handle] failed');
             }
         }
 
