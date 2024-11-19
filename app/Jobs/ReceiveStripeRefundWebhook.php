b8b9e38c22a13d99ec249fac4b64fcd6d0137b88
Brandon <schwartzb4@mymacewan.ca>
2024-11-19 11:21:36 -0700

    add missing import

--- app/Jobs/ReceiveStripeRefundWebhook.php
+++ app/Jobs/ReceiveStripeRefundWebhook.php
@@ -3,6 +3,7 @@
 namespace App\Jobs;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
